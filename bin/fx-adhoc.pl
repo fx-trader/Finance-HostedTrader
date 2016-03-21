2b18ca94c175bf2c103bce5b121be40d53cbbfda
João Costa <joaocosta@zonalivre.org>
2016-03-21 23:30:24 +0000

    adhoc example

---
+++ bin/fx-adhoc.pl
@@ -0,0 +1,25 @@
+#!/usr/bin/perl
+
+# PODNAME: fx-adhoc.pl
+# ABSTRACT: Sample adhoc examples
+
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+use Finance::FXCM::Simple;
+use Data::Printer;
+
+    my $symbol = "AUD/USD";
+    my $direction = "long";
+
+    my $ff = Finance::FXCM::Simple->new(
+                $ENV{FXCM_USER},
+                $ENV{FXCM_PASSWORD},
+                "Real",
+                "http://www.fxcorporate.com/Hosts.jsp");
+
+# Print AUD/USD trades only, sorted by open price
+    my @trades = sort { $a->{openPrice} <=> $b->{openPrice} } grep { $_->{symbol} eq $symbol && $_->{direction} eq $direction } @{ $ff->getTrades };
+    p @trades;
+
