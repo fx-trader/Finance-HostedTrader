f3e7190ba58b48cf50205183c83054522cb79b9a
João Costa <joaocosta@zonalivre.org>
2016-03-26 03:00:09 +0000

    fx-sniper changes

--- bin/fx-sniper.pl
+++ bin/fx-sniper.pl
@@ -6,6 +6,7 @@ use warnings;
 package main;
 
 use Log::Log4perl;
+use List::Util qw(sum);
 
 use Finance::HostedTrader::ExpressionParser;
 use Finance::FXCM::Simple;
@@ -16,12 +17,12 @@ log4perl rootLogger = DEBUG, LOG1, SCREEN
 log4perl.appender.SCREEN         = Log::Log4perl::Appender::Screen
 log4perl.appender.SCREEN.stderr  = 0
 log4perl.appender.SCREEN.layout  = Log::Log4perl::Layout::PatternLayout
-log4perl.appender.SCREEN.layout.ConversionPattern = %d %m %n
+log4perl.appender.SCREEN.layout.ConversionPattern = %d{ISO8601} %m %n
 log4perl.appender.LOG1 = Log::Log4perl::Appender::File
 log4perl.appender.LOG1.filename = ./sniper.log
 log4perl.appender.LOG1.mode = append
 log4perl.appender.LOG1.layout = Log::Log4perl::Layout::PatternLayout
-log4perl.appender.LOG1.layout.ConversionPattern = %d %p %m %n
+log4perl.appender.LOG1.layout.ConversionPattern = %d{ISO8601} %p %M{2} %m %n
 );
 Log::Log4perl::init(\$log_conf);
 my $logger = Log::Log4perl->get_logger();
@@ -66,33 +67,42 @@ my %rules = (
 $logger->logdie("FXCM_USERNAME NOT DEFINED") if (!$ENV{FXCM_USERNAME});
 $logger->logdie("FXCM_PASSWORD NOT DEFINED") if (!$ENV{FXCM_PASSWORD});
 
-my $symbol = "AUDUSD";
-my $fxcm_symbol = "AUD/USD";
-my $signal_processor = Finance::HostedTrader::ExpressionParser->new();
-
+my $symbol = "AUDUSD";          # The symbol to trade in
+my $fxcm_symbol = "AUD/USD";    # Finance::FXCM::Simple only knows about FXCM symbols which have a different format than Finance::HostedTrader symbols
+my $max_exposure = 50000;       # Maximum amount I'm willing to buy/sell in $symbol
+my $exposure_increment = 1000;  # How much more do I want to buy each time
+my $check_interval = 5;        # How many seconds to wait for before checking again if it's time to buy
 
 while (1) {
     last if ( -f "/tmp/sniper_disengage" );
 
-    sleep(3);
+    sleep($check_interval);
 
     my $data = getIndicatorValue($symbol, '4hour', "macd(close, 12, 26, 9) - macdsig(close, 12, 26, 9)");
     $logger->debug("Skip") and next if ($data->[1] >= 0);
 
     $data = getIndicatorValue($symbol, '5min', "rsi(close,14)");
-    $logger->debug("Skip") and next if ($data->[1] >= 65);
+    $logger->debug("Skip") and next if ($data->[1] >= 35);
 
     my $fxcm = Finance::FXCM::Simple->new($ENV{FXCM_USERNAME}, $ENV{FXCM_PASSWORD}, 'Real', 'http://www.fxcorporate.com/Hosts.jsp');
-    my @trades = sort { $b->{openPrice} <=> $a->{openPrice} } grep { $_->{symbol} eq $fxcm_symbol && $_->{direction} eq 'long' } @{ $fxcm->getTrades() || [] };
+    my $symbol_trades = $fxcm->getTradesForSymbol($fxcm_symbol);
+    my $symbol_exposure = sum map { $_->{direction} eq 'long' ? $_->{size} : $_->{size} * (-1) }  @$symbol_trades;
 
-    $logger->debug("LAST TRADE = " . $trades[0]->{openPrice}) if ($trades[0]);
-    $logger->debug("ASK = " . $fxcm->getAsk($fxcm_symbol));
-    $logger->debug("BID = " . $fxcm->getBid($fxcm_symbol));
+    my @trades = sort { $b->{openDate} cmp $a->{openDate} } grep { $_->{direction} eq 'long' } @{ $symbol_trades || [] };
+    $logger->debug("LAST TRADE = " . $trades[0]->{openPrice}) if ( $trades[0]);
+    $logger->debug("ASK = " . $fxcm->getAsk($fxcm_symbol)); # The price I can buy at
 
-    $fxcm = undef;
+    my $latest_price = $fxcm->getAsk($fxcm_symbol);
+    $logger->debug("Skip") and next if ( $trades[0] && ( $latest_price < $trades[0]->{openPrice} - 25 ) );
 
-    $logger->debug("Should we buy here ?");
+    $logger->debug("Max Exposure = $max_exposure");
+    $logger->debug("Current Exposure = $symbol_exposure");
+    $logger->debug("Increment = $exposure_increment");
+    $logger->debug("Skip") and next if ( $max_exposure < $symbol_exposure + $exposure_increment);
 
+    $logger->debug("Should we buy $exposure_increment here ?");
+
+    $fxcm = undef; #This logouts from the FXCM session, and can take a few seconds to return
 }
 
 sub getIndicatorValue {
@@ -100,6 +110,7 @@ sub getIndicatorValue {
     my $tf = shift;
     my $indicator = shift;
 
+    my $signal_processor = Finance::HostedTrader::ExpressionParser->new(); # This object knows how to calculate technical indicators
     my $data = $signal_processor->getIndicatorData( {
             'fields'          => "datetime,$indicator",
             'symbol'          => $symbol,
@@ -107,6 +118,7 @@ sub getIndicatorValue {
             'maxLoadedItems'  => 5000,
             'numItems' => 1,
         });
+    $signal_processor = undef;
     $logger->logdie("Failed to retrieve indicator '$indicator'") if (!$data || !$data->[0]);
     $logger->debug("[$data->[0]->[0]] $indicator = $data->[0]->[1]");
 
