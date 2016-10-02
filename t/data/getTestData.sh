865d71482966a4dce7417afe4a66d8547ecbf6bc
João Costa <joaocosta@zonalivre.org>
2016-10-02 00:22:24 +0000

    Document docker differences

--- t/data/getTestData.sh
+++ t/data/getTestData.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 #simple script to export data needed by the tests
 
+# As written, this can only run inside the container, and the container needs to have the mariadb-client package installed (apt-get update && apt-get install mariadb-client)
+
 set -eu
 
 TEST_DATA_DIR=${1:-.}
@@ -11,6 +13,7 @@ TFS="300"
 DBUSER=`fx-show-config.pl db::dbuser`
 DBPASSWD=`fx-show-config.pl db::dbpasswd`
 DBNAME=`fx-show-config.pl db::dbname`
+DBHOST=`fx-show-config.pl db::dbhost`
 
 cd "$(dirname "$0")"
 
@@ -21,7 +24,7 @@ cd testdata
 for sym in $SYMS; do
   for tf in $TFS; do
     echo Dumping $sym $tf
-    mysql -h fxdata -u$DBUSER -p$DBPASSWD -e "SELECT * FROM ${sym}_${tf} WHERE datetime between '2016-02-26 00:00:00' AND '2016-10-02 00:00:00'" $DBNAME | gzip > ${sym}_${tf}.gz
+    mysql -h $DBHOST -u$DBUSER -p$DBPASSWD -e "SELECT * FROM ${sym}_${tf} WHERE datetime between '2016-02-26 00:00:00' AND '2016-10-02 00:00:00'" $DBNAME | gzip > ${sym}_${tf}.gz
   done
 done
 
--- t/data/loadTestData.sh
+++ t/data/loadTestData.sh
@@ -5,6 +5,8 @@ set -eu
 
 TEST_DATA_DIR=${1:-.}
 
+# As written, this can only run inside the container, and the container needs to have the mariadb-client package installed (apt-get update && apt-get install mariadb-client)
+
 # Could drop database here, recreate it, then load test dataset, ie:
 # docker run --rm --link fxdata:mysql mariadb sh -c 'exec mysql -h"$MYSQL_PORT_3306_TCP_ADDR" -P"$MYSQL_PORT_3306_TCP_PORT" -uroot -p"$MYSQL_ENV_MYSQL_ROOT_PASSWORD" -e "drop database fxdata"'
 # docker run --rm --link fxdata::fxdata -v /root/fxtrader.cfg:etc/fxtrader fxtrader/scripts 'fx-create-db-schema.pl | fx-db-client.pl'
@@ -13,6 +15,7 @@ TEST_DATA_DIR=${1:-.}
 DBUSER=`fx-show-config.pl db::dbuser`
 DBPASSWD=`fx-show-config.pl db::dbpasswd`
 DBNAME=`fx-show-config.pl db::dbname`
+DBHOST=`fx-show-config.pl db::dbhost`
 
 rm -vfR testdata
 tar vxf ${TEST_DATA_DIR}/testdata.tar
@@ -22,6 +25,6 @@ for file in *gz; do
     gunzip -v $file
 done
 
-mysqlimport -h fxdata -v --ignore --ignore-lines=1 --local --fields-terminated-by='\t' --lines-terminated-by='\n' -s -u$DBUSER -p$DBPASSWD $DBNAME *_*
+mysqlimport -h $DBHOST -v --ignore --ignore-lines=1 --local --fields-terminated-by='\t' --lines-terminated-by='\n' -s -u$DBUSER -p$DBPASSWD $DBNAME *_*
 cd ..
 rm -vfR testdata
