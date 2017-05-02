255fcd5890944161d7d6dbe82910f5ec62f422a4
João Costa <joaocosta@zonalivre.org>
2017-05-02 23:14:53 +0000

    Refactor argument names for consistency

--- bin/fx-test-signal.pl
+++ bin/fx-test-signal.pl
@@ -1,10 +1,10 @@
 #!/usr/bin/perl
-# ABSTRACT: Outputs the value of a signal against all known symbols
+# ABSTRACT: Outputs the value of a signal against all known instruments
 # PODNAME: fx-test-signal.pl
 
 =head1 SYNOPSIS
 
-    fx-test-signal.pl [--timeframe=tf] [--verbose] [--symbols=s] [--debug] [--maxLoadedItems=i] [--numItems=i] expr
+    fx-test-signal.pl [--timeframe=tf] [--verbose] [--instruments=s] [--debug] [--max_loaded_items=i] [--item_count=i] expr
 
 
 =head1 DESCRIPTION
@@ -24,10 +24,9 @@ Required argument. Specifies a single timeframe
 
 tf can be a valid integer timeframe as defined in L<Finance::HostedTrader::Datasource>
 
-=item C<--symbols=s>
+=item C<--instruments=s>
 
-Comma separated list of symbols for which to run the indicator against.
-If not supplied, defaults to the list entry in the config file item "symbols.synthetic" and "symbols.natural".
+Comma separated list of instruments for which to run the indicator against.
 
 =item C<--help>
 
@@ -37,11 +36,11 @@ Display usage information.
 
 Debug output. TODO, set log4perl to debug and output to STDERR.
 
-=item C<--maxLoadedItems=i>
+=item C<--max_loaded_items=i>
 
 Number of database records to load. Defaults to 1000 which should be enough to calculate any indicator.
 
-=item C<--numItems=i>
+=item C<--item_count=i>
 
 Number of signal periods to display. By default, all signals in studied period will be displayed.
 
@@ -72,16 +71,16 @@ use Date::Manip;
 use Getopt::Long;
 use Pod::Usage;
 
-my ( $timeframe, $max_loaded_items, $symbols_txt, $debug, $help, $startPeriod, $endPeriod, $numItems, $verbose ) =
+my ( $timeframe, $max_loaded_items, $instruments_txt, $debug, $help, $startPeriod, $endPeriod, $item_count, $verbose ) =
   ( 'day', undef, '', 0, 0, '90 days ago', undef, undef, 0 );
 
 GetOptions(
     "timeframe=s"         => \$timeframe,
-    "numItems=i"         => \$numItems,
+    "item_count=i"         => \$item_count,
     "debug"               => \$debug,
     "verbose"               => \$verbose,
-    "symbols=s"           => \$symbols_txt,
-    "maxLoadedItems=i"  => \$max_loaded_items,
+    "instruments=s"           => \$instruments_txt,
+    "max_loaded_items=i"  => \$max_loaded_items,
     "start=s" => \$startPeriod,
     "end=s" => \$endPeriod,
 ) || pod2usage(2);
@@ -90,19 +89,19 @@ pod2usage(1) if ($help);
 my $cfg               = Finance::HostedTrader::Config->new();
 my $signal_processor = Finance::HostedTrader::ExpressionParser->new();
 
-my $symbols = $cfg->symbols->natural;
+my $instruments = $cfg->symbols->natural;
 
-$symbols = [ split( ',', $symbols_txt ) ] if ($symbols_txt);
+$instruments = [ split( ',', $instruments_txt ) ] if ($instruments_txt);
 
 
 foreach my $signal (@ARGV) {
 print "$signal\n----------------------\n" if ($verbose);
-foreach my $symbol ( @{$symbols} ) {
+foreach my $symbol ( @{$instruments} ) {
     print "Testing $symbol\n" if ($verbose);
     my $signal_args = 
         {
             'expression'        => $signal,
-            'item_count'        => $numItems,
+            'item_count'        => $item_count,
             'symbol'            => $symbol,
             'timeframe'         => $timeframe,
             'max_loaded_items'  => $max_loaded_items,
