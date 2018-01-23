751f1f9463dda671d3a1dd4def1d9143d53209a7
João Costa <joaocosta@zonalivre.org>
2018-01-23 15:19:54 +0000

    Adjust to new directory structure

--- t/data/loadTestData.sh
+++ t/data/loadTestData.sh
@@ -9,7 +9,7 @@ TEST_DATA_DIR=${1:-.}
 
 # Could drop database here, recreate it, then load test dataset, ie:
 # docker run --rm --link fxdata:mysql mariadb sh -c 'exec mysql -h"$MYSQL_PORT_3306_TCP_ADDR" -P"$MYSQL_PORT_3306_TCP_PORT" -uroot -p"$MYSQL_ENV_MYSQL_ROOT_PASSWORD" -e "drop database fxdata"'
-# docker run --rm --link fxdata::fxdata -v /root/fxtrader.cfg:etc/fxtrader fxtrader/finance-hostedtrader 'fx-create-db-schema.pl | fx-db-client.pl'
+# docker run --rm --link fxdata::fxdata -v /root/fx/cfg:etc/fxtrader fxtrader/finance-hostedtrader 'fx-create-db-schema.pl | fx-db-client.pl'
 # Problem with this approach is that data gets old over time, and it's hard to compare visually against charts, because i have no charting capability, and the web charts will tend to have fresh recent data
 
 DBUSER=`fx-show-config.pl db::dbuser`
