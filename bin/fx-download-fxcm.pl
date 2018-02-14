ff0136501e775d37706b386ec78610344faa121c
João Costa <joaocosta@zonalivre.org>
2018-02-14 22:26:06 +0000

    Change data download script to use generic data provider interface

--- bin/fx-download-fxcm.pl
+++ bin/fx-download-fxcm.pl
@@ -13,110 +13,10 @@ $|=1;
 use Getopt::Long;
 use Finance::HostedTrader::Config;
 use Finance::HostedTrader::Datasource;
-use Finance::FXCM::Simple;
+use Finance::HostedTrader::DataProvider;
 use Finance::HostedTrader::Synthetics;
 use Pod::Usage;
 use Try::Tiny;
-
-my %symbolMap = (
-    AUDCAD => 'AUD/CAD',
-    AUDCHF => 'AUD/CHF',
-    AUDJPY => 'AUD/JPY',
-    AUDNZD => 'AUD/NZD',
-    AUDUSD => 'AUD/USD',
-    AUS200 => 'AUS200',
-    CADCHF => 'CAD/CHF',
-    CADJPY => 'CAD/JPY',
-    CHFJPY => 'CHF/JPY',
-    CHFNOK => 'CHF/NOK',
-    CHFSEK => 'CHF/SEK',
-    EURAUD => 'EUR/AUD',
-    EURCAD => 'EUR/CAD',
-    EURCHF => 'EUR/CHF',
-    EURDKK => 'EUR/DKK',
-    EURGBP => 'EUR/GBP',
-    EURJPY => 'EUR/JPY',
-    EURNOK => 'EUR/NOK',
-    EURNZD => 'EUR/NZD',
-    EURSEK => 'EUR/SEK',
-    EURTRY => 'EUR/TRY',
-    EURUSD => 'EUR/USD',
-    GBPAUD => 'GBP/AUD',
-    GBPCAD => 'GBP/CAD',
-    GBPCHF => 'GBP/CHF',
-    GBPJPY => 'GBP/JPY',
-    GBPNZD => 'GBP/NZD',
-    GBPSEK => 'GBP/SEK',
-    GBPUSD => 'GBP/USD',
-    HKDJPY => 'HKD/JPY',
-    NOKJPY => 'NOK/JPY',
-    NZDCAD => 'NZD/CAD',
-    NZDCHF => 'NZD/CHF',
-    NZDJPY => 'NZD/JPY',
-    NZDUSD => 'NZD/USD',
-    SEKJPY => 'SEK/JPY',
-    SGDJPY => 'SGD/JPY',
-    TRYJPY => 'TRY/JPY',
-    USDCAD => 'USD/CAD',
-    USDCHF => 'USD/CHF',
-    USDDKK => 'USD/DKK',
-    USDHKD => 'USD/HKD',
-    USDJPY => 'USD/JPY',
-    USDMXN => 'USD/MXN',
-    USDNOK => 'USD/NOK',
-    USDSEK => 'USD/SEK',
-    USDSGD => 'USD/SGD',
-    USDTRY => 'USD/TRY',
-    USDZAR => 'USD/ZAR',
-    XAGUSD => 'XAG/USD',
-    XAUUSD => 'XAU/USD',
-    ZARJPY => 'ZAR/JPY',
-    ESP35  => 'ESP35',
-    FRA40  => 'FRA40',
-    GER30  => 'GER30',
-    HKG33  => 'HKG33',
-    ITA40  => 'ITA40',
-    JPN225 => 'JPN225',
-    NAS100 => 'NAS100',
-    SPX500 => 'SPX500',
-    SUI30  => 'SUI30',
-    SWE30  => 'SWE30',
-    UK100  => 'UK100',
-    UKOil  => 'UKOil',
-    US30   => 'US30',
-    USOil  => 'USOil',
-    Copper => 'Copper',
-    XPTUSD => 'XPT/USD',
-    XPDUSD => 'CPD/USD',
-    USDOLLAR=> 'USDOLLAR',
-    NGAS    => 'NGAS',
-    EUSTX50 => 'EUSTX50',
-    Bund    => 'Bund',
-);
-
-my %timeframeMap = (
-    60     => 'm1',
-    300    => 'm5',
-    3600   => 'H1',
-    86400  => 'D1',
-    604800 => 'W1',
-);
-
-
-sub convertSymbolToFXCM {
-    my ($symbol) = @_;
-
-    die("Unsupported symbol '$symbol'") if (!exists($symbolMap{$symbol}));
-    return $symbolMap{$symbol};
-}
-
-sub convertTimeframeToFXCM {
-    my ($timeframe) = @_;
-
-    die("Unsupported timeframe '$timeframe'") if (!exists($timeframeMap{$timeframe}));
-    return $timeframeMap{$timeframe};
-}
-
 
 
 my $numItemsToDownload = 10;
@@ -136,9 +36,8 @@ pod2usage(1) if ( $help || !defined($timeframes_from_txt));
 my $cfg = Finance::HostedTrader::Config->new();
 my @symbols = ( $symbols_from_txt ? split(',', $symbols_from_txt) : @{ $cfg->symbols->natural } );
 my @timeframes  = sort split(',', $timeframes_from_txt);
-my $providerCfg = $cfg->tradingProviders->{fxcm};
 
-my $sleep_interval = $ENV{"FXCM_DOWNLOAD_INTERVAL"} // 300;
+my $sleep_interval = $ENV{"DATA_DOWNLOAD_INTERVAL"} // 300;
 
 
 # If the number of items being downloaded is small (and fast to download), keep a globally scoped database connection open
@@ -150,16 +49,15 @@ while (1) {
 
 if (!$service || download_data()) {
 
-    my $fxcm = Finance::FXCM::Simple->new($providerCfg->username, $providerCfg->password, $providerCfg->accountType, $providerCfg->serverURL);
+    my $data_provider = Finance::HostedTrader::DataProvider->factory('FXCM');
     foreach my $timeframe (@timeframes) {
-        my $fxcmTimeframe = convertTimeframeToFXCM($timeframe);
 
         foreach my $symbol (@symbols) {
             print "Fetching $symbol $timeframe\n" if ($verbose);
             my $tableToLoad = $symbol . '_' . $timeframe;
 
             try {
-                $fxcm->saveHistoricalDataToFile($tableToLoad, convertSymbolToFXCM($symbol), $fxcmTimeframe, $numItemsToDownload);
+                $data_provider->saveHistoricalDataToFile($tableToLoad, $symbol, $timeframe, $numItemsToDownload);
                 my $ds = (defined($global_ds) ? $global_ds :  Finance::HostedTrader::Datasource->new());
                 $ds->dbh->do("LOAD DATA LOCAL INFILE '$tableToLoad' IGNORE INTO TABLE $tableToLoad FIELDS TERMINATED BY '\t' LINES TERMINATED BY '\n'") or die($!);
                 unlink($tableToLoad);
@@ -169,7 +67,7 @@ if (!$service || download_data()) {
         }
     }
 
-    $fxcm = undef;
+    $data_provider = undef;
 
     if ($mode eq 'simple') {
         my $symbols = $cfg->symbols->natural();
