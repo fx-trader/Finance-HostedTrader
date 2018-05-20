59312a5f9bffd2c99715e9eec4ebe9a6f2216fb4
João Costa <joaocosta@zonalivre.org>
2018-05-20 11:37:49 +0000

    Parameterise fx-stats script

--- bin/fx-stats.pl
+++ bin/fx-stats.pl
@@ -1,4 +1,12 @@
 #!/usr/bin/perl
+# ABSTRACT: Outputs descriptive statistics for a given instrument
+# PODNAME: fx-stats.pl
+
+=head1 SYNOPSIS
+
+    fx-stats.pl [--timeframe=tf] [--instrument=s] [--max_loaded_items=i] [--item_count=i] [expr]
+
+=cut
 
 use strict;
 use warnings;
@@ -8,18 +16,32 @@ use Finance::HostedTrader::Config;
 use Finance::HostedTrader::ExpressionParser;
 
 use Data::Dumper;
+use Getopt::Long;
+use Pod::Usage;
+
+my ( $timeframe, $max_loaded_items, $max_display_items, $instrument, $help, $man ) =
+  ( 'day', 180000, 625, 'GBPJPY', 0 );
+
+GetOptions(
+    "timeframe=s"           => \$timeframe,
+    "instrument=s"          => \$instrument,
+    "max_loaded_items=i"    => \$max_loaded_items,
+    "item_count=i"          => \$max_display_items,
+    "help|?"                => \$help,
+    "man"                   => \$man,
+) || pod2usage(2);
+pod2usage(1) if ($help);
+pod2usage(-exitval => 0, -verbose => 2) if ($man);
 
 my $db  = Finance::HostedTrader::Datasource->new();
 my $cfg = $db->cfg;
 my $signal_processor = Finance::HostedTrader::ExpressionParser->new($db);
 
 
-my $timeframe           = 'day';
-my $instrument          = 'GBPJPY';
-my $max_display_items   = 625;
-my $max_loaded_items    = 180000;
+my $expression = $ARGV[0];
 
 my $params = {
+    'expression'        => $expression,
     'timeframe'         => $timeframe,
     'max_loaded_items'  => $max_loaded_items,
     'item_count'        => $max_display_items,
