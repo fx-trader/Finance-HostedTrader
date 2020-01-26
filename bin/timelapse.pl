06ce98057ca2c85d3c718b830af157c1384ab8fb
João Costa <joaocosta@zonalivre.org>
2020-01-26 18:12:18 +0000

    Take into account pip location for position size

--- bin/timelapse.pl
+++ bin/timelapse.pl
@@ -4,6 +4,11 @@ use strict;
 use warnings;
 
 use Finance::HostedTrader::Datasource;
+use Finance::HostedTrader::Config;
+
+my $oanda = Finance::HostedTrader::Config->new()->providers->{oanda};
+my $instruments = $oanda->getInstrumentsFromProvider;
+
 use Log::Log4perl;
 use Date::Manip;
 use Statistics::Descriptive;
@@ -32,39 +37,71 @@ my $lookback = 300;
 $l->info("Start");
 
 my %account = ( balance => 10000, currency => 'GBP' );
-
+my %system = ( trades => [] );
 my @trades;
+
 while ($lookback > 0) {
     my %trade;
     my $date = UnixDate(ParseDate("sunday $lookback weeks ago at midnight"), "%Y-%m-%d 00:00:00");
-    #my $data = $dbh->selectrow_arrayref("SELECT label, RSI14 FROM oanda_historical_ALL_604800 WHERE DATETIME = '$date' AND RSI14 IS NOT NULL ORDER BY abs(50-RSI14) DESC LIMIT 1");
-    my $data = ['NAS100_USD', 70];
+    my $data = $dbh->selectrow_arrayref("SELECT label, RSI14 FROM oanda_historical_ALL_604800 WHERE DATETIME = '$date' AND RSI14 IS NOT NULL ORDER BY abs(50-RSI14) DESC LIMIT 1");
+    #my $data = ['NAS100_USD', 70];
     my $instrument = $data->[0];
     my $rsi_level = $data->[1];
     $l->info($date, "\t", join("\t", @$data));
     $lookback--;
 
+    if (!$instruments->{$instrument}) {
+        $l->warn("Provider does not support $instrument, skipping trade");
+        next;
+    }
+
     my $start_date = UnixDate(ParseDate("sunday $lookback weeks ago at midnight"), "%Y-%m-%d 00:00:00");
     my $open = get_open($instrument, $start_date);
     if (!$open) {
-        $l->info("Not opening trade for $instrument");
+        $l->warn("Not opening trade for $instrument because open signal was not triggered in the requested period");
         next;
     }
 
     $trade{instrument} = $instrument;
+
+    my $size;
     $trade{open} = { datetime => $open->[0], price => $open->[1], size => ( $rsi_level > 50 ? 1 : -1 ) };
-    $l->info("Open $trade{open}{size} units of $instrument on $trade{open}{datetime} at price $trade{open}{price}");
+    #$l->info("Open $trade{open}{size} units of $instrument on $trade{open}{datetime} at price $trade{open}{price}");
 
     get_close(\%trade, $start_date);
-    $l->info("Close $instrument on $trade{close}{datetime} at price $trade{close}{price}");
 
-    my $pl = ($trade{close}->{price} - $trade{open}->{price}) * $trade{open}->{size};
+    my $pl = ps( $instruments->{$instrument}{pipLocation} ) * ($trade{close}->{price} - $trade{open}->{price}) * $trade{open}->{size};
+    $l->info("$instrument, OpenDate = $trade{open}{datetime}, OpenPrice = $trade{open}{price}, PositionSize = $trade{open}{size}, CloseDate = $trade{close}{datetime}, Close Price = $trade{close}{price}, pl = $pl");
     $trade{stats} = { return => $pl / $account{balance}, pl => $pl};
-    push @trades, \%trade;
+    push @{ $system{trades} }, \%trade;
 }
 
-print Dumper(\@trades);
-my $analysis = analyse_trades(\@trades);
+sub ps {
+    my $pipLocation = shift;
+
+    if (!defined($pipLocation)) {
+        $l->logconfess("undef");
+    }
+
+    if ($pipLocation == -4) {
+        return 10000;
+    } elsif ($pipLocation == -3) {
+        return 1000;
+    } elsif ($pipLocation == -2) {
+        return 100;
+    } elsif ($pipLocation == -1) {
+        return 10;
+    } elsif ($pipLocation == 0) {
+        return 1;
+    } elsif ($pipLocation == 1) {
+        return 0.1;
+    } else {
+        $l->logconfess("Don't know how to calculate pipLocation = $pipLocation");
+    }
+}
+
+print Dumper(\%system);
+my $analysis = analyse_trades($system{trades});
 print Dumper($analysis);
 
 $l->info("Done");
@@ -165,10 +202,10 @@ sub analyse_trades {
         neutral     => scalar(@neutral),
         win_percent => scalar(@winners) / scalar(@$trades),
         loss_percent=> scalar(@loosers) / scalar(@$trades),
-        average_win => $positive_pl->mean(),
-        average_loss=> $negative_pl->mean(),
-        largest_win => $positive_pl->max(),
-        largest_loss=> $negative_pl->min(),
+        average_win => $positive_pl->mean() // 0,
+        average_loss=> $negative_pl->mean() // 0,
+        largest_win => $positive_pl->max() // 0,
+        largest_loss=> $negative_pl->min() // 0,
     };
 
     my $initial_balance = $account{balance};
@@ -199,8 +236,8 @@ sub analyse_trades {
         trades  => $trade_stats,
         system  => $system_stats,
         ratios  => {
-            'sharpe'    => $system_stats->{return} / $system_stats->{volatility},
-            'sortino'   => $system_stats->{return} / $negative_returns->standard_deviation,
+            'sharpe'    => ($system_stats->{volatility} ? $system_stats->{return} / $system_stats->{volatility} : undef),
+            'sortino'   => ($negative_returns->standard_deviation ? $system_stats->{return} / $negative_returns->standard_deviation : undef),
         },
     };
     return $stats;
