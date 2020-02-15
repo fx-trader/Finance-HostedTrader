4e675b953f38552b52e2da7a1784e5bd7f125fff
João Costa <joaocosta@zonalivre.org>
2020-02-15 21:54:58 +0000

    Use ExpressionParser to calculate multiple timeframes

--- bin/multiple_timeframe_analysis.pl
+++ bin/multiple_timeframe_analysis.pl
@@ -4,21 +4,23 @@ use strict;
 use warnings;
 use Data::Dumper;
 
-use Finance::HostedTrader::Synthetics;
-use Finance::HostedTrader::Datasource;
+use Finance::HostedTrader::ExpressionParser;
 
-my $instrument = 'XAU_EUR';
-my $sql = Finance::HostedTrader::Synthetics::multiple_timeframes(
-    instrument => $instrument,
-    timeframes => {
-        60   => {filter => "< 30"},
-        300  => {filter => "< 30"},
-        900  => {filter => "< 30"},
-        1800 => {filter => "< 30"},
-    }
-);
+my $parser = Finance::HostedTrader::ExpressionParser->new();
 
-my $dbh = Finance::HostedTrader::Datasource->new()->dbh;
-my $result = $dbh->selectall_arrayref($sql);
+my $expression = "datetime,rsi(close,14)";
+   $expression = "datetime,4hour(rsi(close,14)),8hour(atr(14)),8hour(sma(close,10))";
+   $expression = "datetime,4hour(rsi(close,14)),30minute(rsi(close,14)),15minute(rsi(close,14)),5minute(rsi(close,14)),1minute(rsi(close,14))";
+my $instrument  = 'EUR_GBP';
+my $timeframe = '';
 
-print Dumper($result);
+    my $args =
+        {
+            'expression'        => $expression,
+            'instrument'        => $instrument,
+            'item_count'        => 5,
+            #'timeframe'         => '4hour',
+            'test'              => 1,
+        };
+    my $result = $parser->getIndicatorData($args);
+    print Dumper($result);
