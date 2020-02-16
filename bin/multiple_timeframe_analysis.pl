d6a6319ab1ea95007e892b71540148c2b71d1331
João Costa <joaocosta@zonalivre.org>
2020-02-16 20:36:33 +0000

    Tweak

--- bin/multiple_timeframe_analysis.pl
+++ bin/multiple_timeframe_analysis.pl
@@ -8,16 +8,17 @@ use Finance::HostedTrader::ExpressionParser;
 
 my $parser = Finance::HostedTrader::ExpressionParser->new();
 
-
-my $expression = "rsi(close,14) < 30 AND 2hour(sma(close,20)) + 5*2hour(atr(14)) < close";
-   $expression = "rsi(close,14) < 30 AND 15minute(rsi(close,14)) < 30 AND 30minute(rsi(close,14)) < 30";
-   $expression = "rsi(close,14) < 30 AND 15minute(rsi(close,14)) < 30 AND 30minute(rsi(close,14)) < 30";
-   $expression = "8hour(sma(close,200) + 5*atr(14)) < close";
-   $expression = "8hour(sma(close,200)) < close";
-#   $expression = "rsi(close,14) < 30 AND (high > close OR low < close)";
 my $instrument = "XAU_EUR";
 my $max_loaded_items = "25000";
 my $timeframe = '5min';
+my $provider = 'oanda_historical';
+
+=pod
+my $expression = "rsi(close,14) < 30 AND 2hour(sma(close,20)) + 5*2hour(atr(14)) < close";
+   $expression = "rsi(close,14) < 30 AND 15minute(rsi(close,14)) < 30 AND 30minute(rsi(close,14)) < 30";
+   $expression = "rsi(close,14) < 30 AND 15minute(rsi(close,14)) < 30 AND 30minute(rsi(close,14)) < 30";
+   $expression = "8hour(sma(close,200)) < close";
+   $expression = "(8hour(sma(close,200)) + 2*8hour(atr(14))) < close AND 4hour(rsi(close,14)) < 30";
 
 my $args =
     {
@@ -25,28 +26,29 @@ my $args =
         'instrument'        => $instrument,
         'timeframe'         => $timeframe,
         'max_loaded_items'  => $max_loaded_items,
-        'provider'          => 'oanda_historical',
+        'provider'          => $provider,
+        'item_count'        => 10,
     };
 
 my $result = $parser->getSignalData( $args );
 
 print "FINAL_RESULT = " . Dumper($result);
+=cut
 
-=pod
 my $expression = "datetime,rsi(close,14)";
    $expression = "datetime,4hour(rsi(close,14)),8hour(atr(14)),8hour(sma(close,10))";
    $expression = "datetime,4hour(rsi(close,14)),30minute(rsi(close,14)),15minute(rsi(close,14)),5minute(rsi(close,14)),1minute(rsi(close,14))";
-my $instrument  = 'EUR_GBP';
-my $timeframe = '';
+   $expression = "datetime,8hour(sma(close,200)),8hour(atr(14))";
+   $expression = "datetime,sma(close,200),atr(14),30minute(atr(14))";
 
     my $args =
         {
             'expression'        => $expression,
             'instrument'        => $instrument,
             'item_count'        => 5,
-            #'timeframe'         => '4hour',
-            'test'              => 1,
+            'timeframe'         => $timeframe,
+            'provider'          => $provider,
+            'max_loaded_items'  => $max_loaded_items,
         };
     my $result = $parser->getIndicatorData($args);
     print Dumper($result);
-=cut
